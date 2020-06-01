683521e0ec3b27720353cc540abc74ea17ec97f8
Hammett <am.hammett@gmail.com>
2020-06-01 11:57:44 +1000

    these tests are bad and you should feel bad

---
+++ test/extract.test.ts
@@ -0,0 +1,8 @@
+import * as chai from 'chai'
+import * as extract from '../src/extract'
+
+describe('this is a bad test', () => {
+  it('and you should feel bad', () => {
+    chai.expect(true).to.be.true
+  })
+})
---
+++ test/index.test.ts
@@ -0,0 +1,8 @@
+import * as chai from 'chai'
+import * as index from '../src/index'
+
+describe('this is a bad test', () => {
+  it('and you should feel bad', () => {
+    chai.expect(true).to.be.true
+  })
+})
